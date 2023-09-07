74ae636c8e99f6adfcc2b34e9126407a983be8f6
Dylan LEFEBVRE <dylan.lefebvre76@live.com>
2023-09-07 07:47:17 +0200

    correction de la structure

---
+++ script.js
@@ -0,0 +1,10 @@
+// Menu pour les mobile
+function openNav() {
+  // Cette fonction ouvre le menu de navigation mobile
+  document.getElementById("myNav_mobile").style.width = "100%";
+}
+
+function closeNav() {
+  // Cette fonction ferme le menu de navigation mobile
+  document.getElementById("myNav_mobile").style.width = "0%";
+}
